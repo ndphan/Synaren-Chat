c9003a918d10270ce588f785e281187349c8974c
Nam Phan <namp323@gmail.com>
2019-04-16 16:59:39 +1000

    add cloudformation template

---
+++ lambda/deploy/deploy.sh
@@ -0,0 +1,7 @@
+aws cloudformation package \
+  --template-file template.yaml \
+  --output-template-file package.yaml \
+  --s3-bucket synaren-cli && \
+aws cloudformation deploy \
+  --template-file package.yaml \
+  --stack-name SynarenChat
