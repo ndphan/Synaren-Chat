131de71eb2530bc7dba35f05332c506ab1930c1b
Nam Phan <namp323@gmail.com>
2019-06-26 19:07:15 +1000

    update cloudformation to provision iam role and remove hardcoded iam

--- lambda/deploy/deploy.sh
+++ lambda/deploy/deploy.sh
@@ -4,4 +4,8 @@ aws cloudformation package \
   --s3-bucket synaren-cli && \
 aws cloudformation deploy \
   --template-file package.yaml \
-  --stack-name SynarenChat
+  --stack-name SynarenChat \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+      DynamodbTableBaseName=chat-session \
+      FunctionBaseName=SynarenChat
